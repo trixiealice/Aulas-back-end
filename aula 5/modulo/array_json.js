c8ad1656cd376d03ff142ac1decfdf10be58aa31
ALICE CAMPOS DA SILVA <25203690@cfp127aluno.local>
2026-03-13 14:05:48 -0300

    Aulas atualizadas

--- aula 5/modulo/array_json.js
+++ aula 5/modulo/array_json.js
@@ -1,57 +1,59 @@
-/*********************************************
- * Objetivo: Manipular dados em ARRAY e JSON
- * Data: 05/03/26
- * Autora: Alice
- * Versão: 1.0
- ********************************************/
+/***************************************************************************************************
+ * Objetivo: Manipular dados em ARRAY E JSON
+ * Autor: Alice
+ * Data: 05/03/26 - 11/03 - 13/03
+ * Versão: 1.0 
+ ***************************************************************************************************/
 
 /*
-    [ ] -> Representa um objeto do tipo ARRAY
-    { } -> Representa um obejto do tipo JSON
+    [ ] -> representa um objeto do tipo ARRAY
+    { } -> Representa um objeto do tipo JSON
 
-    Array -> É um espaço na memória para armazenar dados sem a nescessidade de criar outros objetos
-        Ex:
-            let nome    = 'Jose'
-            let nome2   = 'Maria'
-            let nome3   = 'João'
+    ARRAY -> É um espaço na memória para armazenar dados sem a necessidade de criar outros objetos
 
-            let nomes = ['Jose, 'Maria', 'João']
+        Ex: 
+        let nome = 'José'
+        let nome = 'Maria'
+        let nome = 'João'
+            
+        indices(posições)     0        1       2
+        let nomes =       ['José', 'Maria', 'João']
 
     JSON -> É um espaço na memória para armazenar dados com CHAVE e VALOR
         Ex:
-            let nome        = 'Jose'
+            let nome        = 'José'
             let telefone    = '123456789'
-            let email       = 'jose@gamil.com'
+            let email       = 'jose@gmail.com'
 
-            let cliente     = {'nome: 'Jose', 'telefone': '123456789', 'email': 'jose@gmail.com'}
+                            Atributo
+                            Chave   Valor     Chave        Valor       Chave     Valor
+            let cliente = ("nome": "José", "telefone": "123456789", "email": "jose@gmail.com")
 
 */
 
 //Criando objetos do tipo ARRAY
-const listaDeAlunos     = ["Jose", "Maria", "Luiz", "Antonio", "Carlos"] 
-const listaDeClientes   = []
-const listaDeFornecedores   = []
+const listaDeAlunos = ["José", "Maria", "Luiz", "Antônio", "Carlos"]
+const listaDeClientes = []
+const listaDeFornecedores = []
 
+const exibirDados = function(){
+    //Exibindo o tipo de dados de um indice
+    console.log(typeof(listaDeAlunos[6]))
 
-const exibirDados = function (){
-
-
-
-
-    console.log(listaDeAlunos)
-    
-    //mostra o ARRAY em forma de tabela, mostrando INDICE e CONTEÚDO
+    //Mostra o array em forma de tabela, mostrando índice e conteúdo
     console.table(listaDeAlunos)
 
+    console.log(listaDeAlunos[3])
+    console.log(listaDeAlunos[0])
+    
     console.log(`O nome do aluno é: ${listaDeAlunos[0]}`)
     console.log(`O nome do aluno é: ${listaDeAlunos[1]}`)
     console.log(`O nome do aluno é: ${listaDeAlunos[2]}`)
     console.log(`O nome do aluno é: ${listaDeAlunos[3]}`)
     console.log(`O nome do aluno é: ${listaDeAlunos[4]}`)
 
-
-    //Usando While
-    console.log('\n******* Exemplo com While ********')
+    console.log("**********Exemplo com WHILE***************")
+    //Usando o While
     let cont = 0
     while(cont<5){
         console.log(`O nome do aluno é: ${listaDeAlunos[cont]}`)
@@ -59,49 +61,50 @@ const exibirDados = function (){
     }
 
     //Usando o FOR
-    console.log('\n******* Exemplo com FOR *******')
-    for(let contador = 0; contador <5; contador++){
+    console.log("**********Exemplo com FOR****************")
+    for(let contador = 0; contador < 5; contador++){
         console.log(`O nome do aluno é: ${listaDeAlunos[contador]}`)
     }
 
-    //Usando FOR EACH
-    console.log('\n******* Exemplo com FOR EACH *******')
-    listaDeAlunos.forEach(function(item){
-        console.log(`O nome do aluno é: ${item}`)
+    //Usando o FOR EACH
+    //Método de array que me devolve o conteúdo por meio de uma função de callback
+    //CALLBACK: chama a função e manda algo pra ela
+    //Ele não faz repetição em Json, somente em array
+    console.log("********Exemplo com FOR EACH ************")
+    listaDeAlunos.forEach(function(aluno){
+        console.log(`O nome do aluno é: ${aluno}`)
     })
 
-
-    //Usando FOR OF
-    console.log('\n ******* Exemplo com FOR OF*******')
+    //Usando o FOR OF
+    console.log("********Exemplo com FOR OF ************")
     for (aluno of listaDeAlunos){
         console.log(`O nome do aluno é: ${aluno}`)
     }
 
-    //Usando FOR IN
-    console.log('\n ******* Exemplo com FOR IN *******')
-    for (let item in listaDeAlunos) {
-        console.log(`O índice do aluno é: ${item}`)
+    //Usando o FOR IN
+    console.log("********Exemplo com FOR IN ************")
+    for(aluno in listaDeAlunos){
+        console.log(`O nome do aluno é: ${listaDeAlunos[item]}`)
     }
 
-    //Retorna a quantidade de item em um ARRAY
-    console.log(listaDeAlunos.length)
-    
+    //retorna a quantidade de items em um array
+    console,log(listaDeAlunos.length)
 }
 
 const manipularDados = function(){
-    listaDeClientes[0] = "José da Silva"
+    listaDeClientes[0] = "Jose da Silva"
     listaDeClientes[1] = "Maria da Silva"
     listaDeClientes[2] = "Luiz da Silva"
     listaDeClientes[3] = "Ana da Silva"
     listaDeClientes[5] = "Beatriz da Silva"
 
     console.log(listaDeClientes)
-    
-    //Permite adiconar nvos elementos no ARRAY sempre no FINAL
-    listaDeFornecedores.push('Antônio')
-    listaDeFornecedores.push('Caio')
-    listaDeFornecedores.push('Luiz')
-    listaDeFornecedores.push('Hugo', 'Maria', 'Jose', 'Andre')
+
+    //Permite adicionar novos elementos no ARRAY, sempre no FINAL
+    listaDeFornecedores.push("Antônio")
+    listaDeFornecedores.push("Caio")
+    listaDeFornecedores.push("Luiz")
+
 
     //Permite adicionar novos elementos no ARRAY, sempre no INICIO
     //Após adicionar o elemento, ele reorganiza todos os outros itens
@@ -130,38 +133,51 @@ const manipularDados = function(){
     console.table(listaDeFornecedores)
 }
 
-const removerItem = function(nomeAluno){
+const removerNome = function(nomeAluno){
+    let cont = 0 
+    let qtde = listaDeAlunos.length
+    
 
-    let cont = 0
-    let qted = listaDeAlunos.length
-
-    while(cont < qted){
+    while(cont < qtde){
         if(nomeAluno == listaDeAlunos[cont]){
             listaDeAlunos.splice(cont, 1)
         }
         cont++
     }
 
-const removerItem = function(nomeAluno){
-
-    for(cont in listaDeAlunos){
-        if(nomeAluno == listaDeAlunos[cont]){
-            listaDeAlunos.splice(cont,1)
-        }
-    }
 }
 
+const verificarItem = function(nomeAluno){
+    //verificar se o conteúdo existe dentro do ARRAY e retorna (true/false)
+    return listaDeAlunos.includes(nomeAluno)
+}
+
+const manipularDadosJSON = function (){
+    //Criando um objeto JSON
+     //A estrutura do JSON é a Chave (atributo) : Valor (conteúdo)
+    let aluno = {"id":1, "nome" :"José da Silva", "ra":123456, "email":"jose@gmail.com"}
+
+    //Adiciona um novo atributo no JSON já existente
+    aluno.telefone = "011-975845252"
+    aluno.data_nascimento = "10/05/2000"
+
+    //Exibe o objeto JSON
+    console.log(aluno)
+
+    //Exibe um atributo só, não o json inteiro
+    console.log(aluno.nome)
+
+    //Remove um atributo do JSON
+    delete aluno.email
+    console.log(aluno)
 
 }
 
+// exibirDados()
 
 console.table(listaDeAlunos)
 
-removerItem('José')
+// manipularDados()
+removerNome('José')
 console.table(listaDeAlunos)
-
-
-//manipularDados()
-//exibirDados()
-
-
+manipularDadosJSON()
